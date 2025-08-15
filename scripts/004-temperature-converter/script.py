5d89da30e9b11c5a5a0fdda0fe84ac5d28218f6e
lizardcat <alex.raza.ke@gmail.com>
2025-08-15 23:59:10 +0300

    Adding file analyzer script contents

---
+++ scripts/004-temperature-converter/script.py
@@ -0,0 +1,7 @@
+temp = input("Please enter your celsius temperature: ")
+
+def temp_converter(temp):
+    F = float(temp) * (9/5) + 32
+    return F
+
+print("Your temperature in F is: " . temp_converter(temp)) 
