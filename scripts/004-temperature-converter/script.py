f5b5f92b40f0382056e32e96186ab5fb341d1dd9
lizardcat <alex.raza.ke@gmail.com>
2025-08-16 04:31:58 +0300

    Fixing temperature converter code

--- scripts/004-temperature-converter/script.py
+++ scripts/004-temperature-converter/script.py
@@ -1,7 +1,7 @@
-temp = input("Please enter your celsius temperature: ")
+temp = float(input("Please enter your celsius temperature: "))
 
-def temp_converter(temp):
-    F = float(temp) * (9/5) + 32
-    return F
+def celsius_to_fahrenheit(celsius):
+    return celsius * (9/5) + 32
 
-print("Your temperature in F is: " . temp_converter(temp)) 
+result = celsius_to_fahrenheit(temp)
+print(f"Your temperature in Fahrenheit is: {result}°F")
