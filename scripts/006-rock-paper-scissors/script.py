fd0225fb9a838625dc4d586c2ad6f015bb23cc0e
lizardcat <alex.raza.ke@gmail.com>
2025-09-29 21:56:44 +0300

    Adding script for rock, paper, scissors game

---
+++ scripts/006-rock-paper-scissors/script.py
@@ -0,0 +1,9 @@
+# A simple rock, paper, scissors game
+
+import random
+import sys
+
+print("Let's play rock, paper, scissors!")
+print("Enter 'r' for rock, 'p' for paper, 's' for scissors, or 'q' for quit!")
+
+userChoice = str(input())
