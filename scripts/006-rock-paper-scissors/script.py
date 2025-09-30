2f0a74b86544c580dd73b45a714ff85afec39211
lizardcat <alex.raza.ke@gmail.com>
2025-09-30 23:27:51 +0300

    Playing around with rock, paper, scissor logic

--- scripts/006-rock-paper-scissors/script.py
+++ scripts/006-rock-paper-scissors/script.py
@@ -3,7 +3,25 @@
 import random
 import sys
 
+rock = 0
+scissors = 0
+paper = 0
+tie = 0
+win = 0
+lose = 0
+
 print("Let's play rock, paper, scissors!")
 print("Enter 'r' for rock, 'p' for paper, 's' for scissors, or 'q' for quit!")
 
 userChoice = str(input())
+
+if userChoice == "q":
+    sys.exit
+elif userChoice == "r":
+    rock += 1
+elif userChoice == "p":
+    paper += 1
+elif userChoice == "s":
+    scissors += 1
+
+botChoice = random.randint(1, 3)
